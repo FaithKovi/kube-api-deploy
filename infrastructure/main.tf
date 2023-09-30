e9e4e23f8967ebc053e6ab71c3e52d6f2db1fa14
FaithKovi <faithkovivera@gmail.com>
2023-09-30 14:15:05 +0100

    add main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -38,7 +38,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  host = data.aws_eks_cluster.cluster.endpoint
+  host = module.eks.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token = data.aws_eks_cluster_auth.cluster.token
   load_config_file = false
